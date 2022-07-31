1c83ef6907076e8e9058bc3888d14d34395fd7ed
max Nguyen <ngmaxime@gmail.com>
2022-07-31 15:01:34 +0200

    test katex

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,19 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+  import Katex from "./lib/Katex.svelte"
+	
+	// math equations
+	const math = "ax^2+bx+c=0";
+	const math2 = "x=-\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}";
 </script>
 
 <main>
 
   <h1>Open your maths</h1>
-
+<div>
+Our math equation: <Katex math={math2}/> and it is inline.
+ </div>
 
 </main>
 
