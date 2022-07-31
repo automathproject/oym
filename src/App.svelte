be6b0a29c4c12a12b325759b256f9a6f3e9f04b9
max Nguyen <ngmaxime@gmail.com>
2022-07-31 14:01:31 +0200

    initialize project

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+
+  <h1>Open your maths</h1>
+
+
+</main>
+
+<style>
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
+</style>
